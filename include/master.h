ee134238d93ac7362148ee9bbffcbec63877688d
Agus Mayor <agustinmayorsaavedra@gmail.com>
2026-08-30 03:40:01 -0300

    agrego header de master

---
+++ include/master.h
@@ -0,0 +1,20 @@
+#ifndef MASTER_H
+#define MASTER_H
+
+#include <stdbool.h>
+
+#define MAX_PLAYERS 9
+
+// struct para los argumentos pasados al master
+typedef struct {
+    unsigned short width, height;
+    unsigned int delay; // ms que espera el master entre cada impresion de estado, default 200 ms
+    unsigned int timeout; // timeout en segundos para recibir solicitudes de movimientos validos, defaut 10 s
+    unsigned int seed; // usada para generar el tablero, default time(NULL)
+    char viewPath[256]; // path del archivo donde se imprimira el estado del juego, default sin vista
+    bool hasView;
+    char * playerPaths[MAX_PLAYERS]; // paths de los ejecutables de los jugadores
+    int cantPlayers;
+} MasterArgs;
+
+#endif
