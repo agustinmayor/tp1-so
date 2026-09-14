7b21c1759c3b99bd776d24ca8b3e5c722520aa27
Agus Mayor <agustinmayorsaavedra@gmail.com>
2026-09-14 14:43:59 -0300

    Update board.h

--- include/board.h
+++ include/board.h
@@ -1,8 +1,6 @@
 #ifndef BOARD_H
 #define BOARD_H
 
-// Geometría del tablero compartida entre el master y los jugadores.
-// Las dos tablas de direcciones tienen que ser idénticas en ambos lados.
 
 #include "game_state.h"
 
