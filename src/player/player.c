645d200294368c4716751b0ee73d78e907cd9e2f
Agus Mayor <agustinmayorsaavedra@gmail.com>
2026-09-14 00:04:37 -0300

    limpio player.c para no repetir codigo

--- src/player/player.c
+++ src/player/player.c
@@ -1,7 +1,7 @@
-#include "game_state.h"
-#include "game_sync.h"
-#include "playerAI.h"
-#include "sharedMem.h"
+// main archivo de los procesos player por IA
+
+#include "playerAI.h" // aca esta chooseMove
+#include "playerUtils.h" // aca esta runPlayerLoop
 
 #include <errno.h>
 #include <signal.h>
@@ -10,96 +10,6 @@
 #include <time.h>
 #include <unistd.h>
 
-#define EXPECTED_ARGC 4
-#define CHECK_MASTER_SECONDS 1
-
-// chequeamos que el master siga vivo para no quedar bloqueados
-// la espera la hacemos x tramos y entre tramo y tranmo chequeamos que el master siga vivo (siga siendo padre)
-static bool waitForMyTurn(sem_t * playerTurn, pid_t masterPid) {
-    while(1) {
-        struct timespec deadline;
-
-        clock_gettime(CLOCK_REALTIME, &deadline);
-
-        deadline.tv_sec += CHECK_MASTER_SECONDS;
-
-        if(sem_timedwait(playerTurn, &deadline) == 0){
-            return true;
-        }
-
-        if(errno == ETIMEDOUT) {
-            if(getppid() != masterPid) {
-                return false;
-            }
-
-            continue;
-        }
-
-        if(errno != EINTR) {
-            return false;
-        }
-
-    }
-}
-
-int main(int argc, char * argv[]) {
-
-    if(argc != EXPECTED_ARGC) {
-        fprintf(stderr, "Uso: %s <width> <height> <playerIndex>\n", argv[0]);
-        return EXIT_FAILURE;
-    }
-
-    unsigned short boardWidth = (unsigned short)strtoul(argv[1], NULL, 10);
-    unsigned short boardHeight = (unsigned short)strtoul(argv[2], NULL, 10);
-    int myIndex = (int)strtol(argv[3], NULL, 10);
-
-    if(boardWidth == 0 || boardHeight == 0 || myIndex < 0 || myIndex >= MAX_PLAYERS) {
-        fprintf(stderr, "Argumentos invalidos: %s <width> <height> <playerIndex>\n", argv[0]);
-        return EXIT_FAILURE;
-    }
-
-    // si el master cierra su extremo del pipe queremos que falle el write y no que nos mate una SIGPIPE
-    signal(SIGPIPE, SIG_IGN);
-
-    size_t gameStateSize = sizeof(GameState) + ((size_t)boardWidth * boardHeight * sizeof(signed char));
-
-    GameState * gs = connectSharedMem(SHM_GAME_STATE_NAME, gameStateSize, SHM_READ_ONLY);
-    GameSync * sync = connectSharedMem(SHM_GAME_SYNC_NAME, sizeof(GameSync), SHM_READ_WRITE);
-
-    GameSnapshot snapshot;
-
-    if(!createSnapshot(&snapshot, boardWidth, boardHeight)) {
-        perror("malloc: Error al reservar la copia del tablero");
-        disconnectSharedMem(sync, sizeof(GameSync));
-        disconnectSharedMem(gs, gameStateSize);
-        return EXIT_FAILURE;
-    }
-
-    pid_t masterPid = getppid();
-
-    while(1) {
-        if(!waitForMyTurn(&sync->playerTurn[myIndex], masterPid)) {
-            break;
-        }
-
-        readerLock(sync);
-        takeSnapshot(&snapshot, gs, myIndex);
-        readerUnlock(sync);
-
-        if(snapshot.isGameOver || snapshot.amIBlocked) {
-            break;
-        }
-
-        unsigned char move = chooseMove(&snapshot, myIndex);
-
-        if(write(STDOUT_FILENO, &move, sizeof(move)) != (ssize_t)sizeof(move)) {
-            break;
-        }
-    }
-
-    destroySnapshot(&snapshot);
-    disconnectSharedMem(sync, sizeof(GameSync));
-    disconnectSharedMem(gs, gameStateSize);
-
-    return EXIT_SUCCESS;
+int main(int argc, char *argv[]) {
+    return runPlayerLoop(argc, argv, chooseMove);
 }
